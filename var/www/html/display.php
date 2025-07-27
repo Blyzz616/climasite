d0e13fa8570ba91889fb8f9543c65e72cf9cadaf
Jim <blyzzcanada@gmail.com>
2025-07-27 15:00:15 -0700

    Create display.php

---
+++ var/www/html/display.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Room Temperature</title>
+  <link rel="stylesheet" href="styles/dark-theme.css">
+</head>
+<body>
+  <h1>Room Temperature</h1>
+  <div id="sensor-data" class="sensor-container"></div>
+
+  <script src="scripts/display.js"></script>
+</body>
+</html>
